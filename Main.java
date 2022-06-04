160e24f6f7611a38a30938fb7e3588fb450596d9
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-06-04 09:40:26 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
-
+public class Main {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello World");    
+    System.out.print("/tIm back\n");   
+    
+    /n
+    // This is a comment
+      /* 
+      * This 
+      * is
+      * a 
+      * comment
+      */   
+  }
+}
